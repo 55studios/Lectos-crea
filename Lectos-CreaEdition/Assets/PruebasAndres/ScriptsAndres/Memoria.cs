b6d79bfab6e7defbd1352bb510fc042ab2627140
Andeca <andeca1990@gmail.com>
2020-10-27 18:03:47 -0500

    Generación de minijuegos y funcionalidad (Drag&drop, memoria)

---
+++ Lectos-CreaEdition/Assets/PruebasAndres/ScriptsAndres/Memoria.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Memoria : MonoBehaviour
+{
+    public GameObject checker;
+    public bool activado;
+
+    private void OnMouseUp()
+    {
+        if (Input.GetMouseButtonUp(0) && !activado)
+        {
+            checker.GetComponent<CheckMemoria>().Activar(gameObject);
+        }
+    }
+}
---
+++ Lectos-CreaEdition/Assets/PruebasAndres/ScriptsAndres/ScriptableScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Nuevo minijuego", menuName = "Minijuego")]
+public class ScriptableScene : ScriptableObject
+{
+    public int puntosLogrables;
+    public string reward;
+    public int tipoMinijuego;
+    public Sprite[] activadores;
+    public Sprite[] receptores;
+    public string[] respuestasTexto;
+}
