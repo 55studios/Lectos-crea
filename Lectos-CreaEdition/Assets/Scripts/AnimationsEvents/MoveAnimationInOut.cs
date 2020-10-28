378c218f9e51311b56ed99c81455854421c799d0
Stiven <stivecreative@gmail.com>
2020-10-28 01:00:31 -0500

    NewFeatures to animation and buttons

---
+++ Lectos-CreaEdition/Assets/Scripts/AnimationsEvents/MoveAnimationInOut.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MoveAnimationInOut : MonoBehaviour
+{
+    public bool onStartAnimation;
+    public float delay = 1;
+    public Ease easeIn = Ease.InOutQuad;
+    public Vector2 moveTo;
+    public Ease easeOut;
+    public Vector2 moveFrom;
+
+    private RectTransform mainTransform;
+
+
+    private void Start() {
+        mainTransform = this.gameObject.GetComponent<RectTransform>();
+        if (onStartAnimation) {
+            mainTransform.DOAnchorPos(moveTo, delay).SetEase(easeIn);
+        }
+    }
+
+    public void InAnimation() {
+
+        mainTransform.DOAnchorPos(moveTo, delay).SetEase(easeIn);
+    }
+
+    public void OutAnimation() {
+
+        mainTransform.DOAnchorPos(moveFrom, delay).SetEase(easeIn);
+    }
+}
