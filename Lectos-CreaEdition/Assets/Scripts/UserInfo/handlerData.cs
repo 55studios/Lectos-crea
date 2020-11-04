fabe99e513116777db1fc6c512e4b82fbccf63fb
Stiven <stivecreative@gmail.com>
2020-11-04 01:22:07 -0500

    corrections animations and first pass for segond period

---
+++ Lectos-CreaEdition/Assets/Scripts/UserInfo/handlerData.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HandlerDataPlanet : MonoBehaviour
+{
+    [HideInInspector]
+    public GalaxiaAData _data;
+
+    [Header("Planet data")]
+    public int planetId;
+    public bool planetIsBlocked;
+    public int totalStars;
+    public int Totalporcent;
+
+    [Header("Objects")]
+    public Button planetButton;
+
+    public void SaveData() {
+        _data = GameObject.Find("DataUser").GetComponent<GalaxiaAData>();
+
+        if (_data == null) {
+            Debug.Log("I couldnt find the reference");
+        }
+        else {
+            _data.PlanetDataSaved(planetId,planetIsBlocked,totalStars,Totalporcent);
+            if (_data.PlanetsV[planetId].Block) {
+                planetButton.interactable = false;
+            }
+            else {
+                planetButton.interactable = true;
+            }
+        }
+    }
+
+    private void Start() {
+        _data = GameObject.Find("DataUser").GetComponent<GalaxiaAData>();
+        LoadDataPlanet();
+    }
+
+    void LoadDataPlanet() {
+        if (_data == null) {
+            Debug.Log("I couldnt find the reference");
+        }
+        else {
+            if (_data.dataloaded) {
+                if (_data.PlanetsV[planetId].Block) {
+                    planetButton.interactable = false;
+                }
+                else {
+                    planetButton.interactable = true;
+                }
+            }
+            else {
+                Debug.Log("Data cant be loaded");
+                
+            }
+        }
+    }
+
+}
