173dd2ca4ce4ed5b3d48373ea1651f2ba3b5acd5
Stiven <stivecreative@gmail.com>
2020-11-06 21:12:07 -0500

    integracion planeta amarillo y modelos de lecto y cribi(aunque cribi esta con errores aun)

--- Lectos-CreaEdition/Assets/Scripts/CHaracters/AnimationSprites.cs
+++ Lectos-CreaEdition/Assets/Scripts/CHaracters/AnimationSprites.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class AnimationSprites : MonoBehaviour {
 
+    [Tooltip("Basado en tiempo real")]
+    public float frameRate;
+    public bool isUi;
+    public Sprite[] animationsSprites;
     public Texture[] animationTextures;
     private MeshRenderer shderTexture;
+    private Image _imageRenderer;
     private bool waveActive = true;
 	void Start ()
     {
-        shderTexture = GetComponent<MeshRenderer>();
-        StartCoroutine(LoopAnimation());
+        if (!isUi) {
+            shderTexture = GetComponent<MeshRenderer>();
+            StartCoroutine(LoopAnimation());
+        }
+        else {
+            _imageRenderer = GetComponent<Image>();
+            StartCoroutine(LoopAnimationSprites());
+        }
+        
 		
 	}
 
@@ -26,7 +38,20 @@ public class AnimationSprites : MonoBehaviour {
                 {
                     i = 0;
                 }
-                yield return new WaitForSeconds(0.1f);
+                yield return new WaitForSeconds(frameRate);
+            }
+        }
+    }
+
+    IEnumerator LoopAnimationSprites() {
+        while (waveActive) {
+
+            for (int i = 0; i < animationsSprites.Length; i++) {
+                _imageRenderer.sprite = animationsSprites[i];
+                if (i == animationsSprites.Length) {
+                    i = 0;
+                }
+                yield return new WaitForSeconds(frameRate);
             }
         }
     }
